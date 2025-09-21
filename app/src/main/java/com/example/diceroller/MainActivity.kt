98c087c391529bf32390972bafe415c752090e1b
Raphael Magaso <20230028448@my.xu.edu.ph>
2025-09-21 19:10:01 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,11 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val diceResult: TextView = findViewById(R.id.dice_result)
+
+
+        rollButton.setOnClickListener {
+            val randomRoll = (1..6).random()
+            diceResult.text = randomRoll.toString()
+        }
+
     }
 }
